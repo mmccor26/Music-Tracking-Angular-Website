60bd27d829dc7326b0504fa0246440995c24e24a
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-11-13 15:50:25 -0500

    Blank EMCA Doc Added, playlist schema modification

--- models/playlist.js
+++ models/playlist.js
@@ -1,8 +1,20 @@
 // JavaScript File
+var Song         = require('./song');
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var PlaylistSchema   = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    songs:{
+        type: [Schema.Types.Song] 
+    }
     
 });
 
---
+++ models/user.js
@@ -0,0 +1,9 @@
+// JavaScript File
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var UserSchema   = new Schema({
+   
+});
+
+module.exports = mongoose.model('User', UserSchema);
