7e438cdb9a4810342bb829ec278d01d9ab139133
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-11-13 21:36:57 -0500

    Added login and register page routing

--- se3316Lab5/src/app/app-routing.module.ts
+++ se3316Lab5/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'Login', component:LoginComponent},
+  {path: 'Register', component:RegisterComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- se3316Lab5/src/app/app.module.ts
+++ se3316Lab5/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
