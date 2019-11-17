cab96360dd35c0345d581cff574d2a3c7117c73f
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-11-16 19:05:19 -0500

    UI to find specified song reviews added

--- se3316Lab5/src/app/home/home.component.ts
+++ se3316Lab5/src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpService } from '../http.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  songReviews:Object;
+  song_id:string;
 
-  constructor() { }
+  constructor(private HttpService: HttpService) { }
 
   ngOnInit() {
+  }
+  getReviews():void{
+    this.HttpService.getReviews(this.song_id).subscribe(data=>{
+      this.songReviews = data;
+    })
+
+    
   }
 
 }
